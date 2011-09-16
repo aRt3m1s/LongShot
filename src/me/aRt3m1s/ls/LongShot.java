031d32adcf2acbdd64b2bedd9ad6016e261d0403
Christian Enano <artemis_fowl@aol.com>
2011-09-16 12:23:38 -0400

    first commit...added all files

---
+++ src/me/aRt3m1s/ls/LongShot.java
@@ -0,0 +1,59 @@
+package me.aRt3m1s.ls;
+
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christian
+ * Date: 9/16/11
+ * Time: 7:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LongShot extends JavaPlugin{
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static final String pPrefix = "[LS]";
+    private final lsEL el = new lsEL(this);
+    Configuration config;
+    /**
+     * Called when this plugin is disabled
+     */
+    @Override
+    public void onDisable() {
+        log.info(getDescription().getName()+" version v"+getDescription().getVersion()+" is Disabled!");
+    }
+
+    /**
+     * Called when this plugin is enabled
+     */
+    @Override
+    public void onEnable() {
+        config = getConfiguration();
+        config.load();
+        config.setHeader("###########################################################",
+                         "# @critical-hits                                          #",
+                         "#   @double: true                                         #",
+                         "#     the damage will be doubled                          #",
+                         "#   @double: false                                        #",
+                         "#     random number will be added to the damage           #",
+                         "#   @random-range                                         #",
+                         "#     if double is false, random number from 0 to range   #",
+                         "#      will be added to the damage                        #",
+                         "# @distance/BLOCKS-damagePlus:                            #",
+                         "#   if 0, critical hits will be enabled                   #",
+                         "#   if >0, will divide the distance by this number and    #",
+                         "#    add it to the damage                                 #",
+                         "###########################################################");
+        config.getBoolean("LongShot.critical-hits.double", true);
+        config.getInt("LongShot.critical-hits.random-range", 6);
+        config.getInt("LongShot.distance/BLOCKS-damagePlus", 10);
+        config.save();
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.ENTITY_DAMAGE, el, Event.Priority.Normal, this);
+        log.info(getDescription().getName()+" version v"+getDescription().getVersion()+" is Enabled!");
+    }
+}
---
+++ src/me/aRt3m1s/ls/lsEL.java
@@ -0,0 +1,60 @@
+package me.aRt3m1s.ls;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityListener;
+
+import java.util.Random;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Christian
+ * Date: 9/16/11
+ * Time: 7:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class lsEL extends EntityListener{
+    public static LongShot plugin;
+    public lsEL(LongShot instance) {
+        plugin = instance;
+    }
+    public void onEntityDamage(EntityDamageEvent event){
+        if(event instanceof EntityDamageByEntityEvent){
+            EntityDamageByEntityEvent ee = (EntityDamageByEntityEvent)event;
+            if(ee.getDamager() instanceof Projectile){
+                Projectile projectile =(Projectile) ee.getDamager();
+                if(projectile.getShooter() instanceof Player){
+                    Player player = (Player) projectile.getShooter();
+                    if(ee.getEntity() instanceof LivingEntity){
+                        LivingEntity damagee = (LivingEntity) ee.getEntity();
+                        double distance = damagee.getLocation().distance(player.getLocation());
+                        int finalDamage = getFinalDamage(player, distance, ee.getDamage());
+                        ee.setDamage(finalDamage);
+                    }
+                }
+            }
+        }
+    }
+
+    private int getFinalDamage(Player player, double distance, int damage) {
+        int dBdP = plugin.config.getInt("LongShot.distance/BLOCKS-damagePlus", 1);
+        boolean dTOF = plugin.config.getBoolean("LongShot.critical-hits.double", true);
+        int range = plugin.config.getInt("LongShot.critical-hits.random-range", 20);
+        if(dBdP>0){
+            damage += (int)distance/dBdP;
+            return damage;
+        }else{
+            if(dTOF){
+                damage *= 2;
+            }else{
+                Random generator2 = new Random( 19580427 );
+                int plusDamage = generator2.nextInt(range);
+                damage += plusDamage;
+            }
+            return damage;
+        }
+    }
+}
